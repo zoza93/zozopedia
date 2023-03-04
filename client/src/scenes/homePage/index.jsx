53befb507f59bbf50abaf4f3fb7277662e827676
Zorana Zaric <zorana.zaric@panonit.com>
2023-03-04 23:25:54 +0100

    user widget

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,10 +1,40 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
 
 const HomePage = () => {
-    return <Box>
-        <Navbar />
-    </Box>
+    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+    const { _id, picturePath } = useSelector((state)=> state.user);
+
+    return (
+        <Box>
+            <Navbar />
+            <Box 
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreen ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+                    <UserWidget userId={_id} picturePath={picturePath}/>
+                </Box>
+                <Box
+                    flexBasis={isNonMobileScreen ? "42%" : undefined}
+                    mt={isNonMobileScreen ? undefined : "2rem"}>
+                    
+                </Box>
+                {isNonMobileScreen && (
+                    //prikazuje se samo na malim ekranima
+                    <Box flexBasis="26%">
+
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    )
 }
 
 export default HomePage;
